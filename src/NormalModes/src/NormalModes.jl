19cb4cc0df6633c7c209c9c92de1acb3153c1697
Moritz Ferdinand Richter <justme@MacBookAir.fritz.box>
2025-09-16 11:26:56 +0200

    Normal mode commented

--- src/NormalModes/src/NormalModes.jl
+++ src/NormalModes/src/NormalModes.jl
@@ -1,35 +1,43 @@
+
+"""
+The module 'NormalModes' contains several functions concerning the normal modes of a harmonic chain, such as
+normal mode quadrature operators, frequencies, and Hamiltonian operators. Additionally, it also provides a
+function to initialize the chain in a normal mode coherent state, i.e., a collective displacement in phase
+space according to the given mode.
+"""
 module NormalModes
 
     import QuantumOptics: Ket, Operator, FockBasis, fockstate, create, destroy, identityoperator, ⊗, dagger, exp, expect, eigenstates, dense, norm
-    export QuadraturesNormalMode, FrequenceNormalMode, NumberoperatorNormalMode, HamiltonianNormalMode, DisplacementoperatorNormalMode, coherentstateNM, fockstateNM, means
+    export QuadraturesNormalMode, FrequenceNormalMode, HamiltonianNormalMode, DisplacementoperatorNormalMode, coherentstateNM, means
 
+    """
+    Position operator of single oscillator
+    """
     function position(basis::FockBasis)
-        """
-        Position operator of single oscillator
-        """
         a = destroy(basis)
         at = create(basis)
         return 0.5( 1/sqrt(2)*(at+a) + dagger(1/sqrt(2)*(at+a)) )
     end
 
+    """
+    Momentum operator of single oscillator
+    """    
     function momentum(basis::FockBasis)
-        """
-        Momentum operator of single oscillator
-        """
         a = destroy(basis)
         at = create(basis)
         return 0.5( -1*im/sqrt(2)*(a-at) + dagger(-1*im/sqrt(2)*(a-at)) )
     end
 
+    """
+    Building a operator at one 'site' of a FPUT chain with 'N' elements. 'singlebasis' is
+    the Fock basis of each single site of the chain.
+    """   
     function operator_at_site(
         O::Operator,   # single oscillator operator
-        N::Int64,      # number of chain elements
+        N::Int64,      # number of sites in chain
         site::Int64,   # site of operator
         )
-        """
-        Building a operator at one 'site' of a FPUT chain with 'N' elements. 'singlebasis' is the Fock basis of each single
-        chain element.
-        """
+
         basis = O.basis_l
         id = identityoperator(basis)
         operatorFPUT = O
@@ -49,14 +57,16 @@ module NormalModes
     end
 
 
+    """
+    Position and momentum operator of the normal mode 'mode' of a harmonic chain with 'N' sites. 
+    'singlebasis' is the Fock basis of each single site of the chain.
+    """
     function QuadraturesNormalMode(
-        N::Int64,  # number of chain elements
+        N::Int64,  # number of sites in chain
         mode::Int64,  # level of normal mode
         singlebasis::FockBasis
         )
-        """
-        Position and momentum operator of the 'k'-th normal mode of a harmonic FPUT chain with 'N' elements
-        """
+
         positionNM = sqrt(2/(N+1)) * operator_at_site(position(singlebasis),N,1) * sin(pi*mode*1/(N+1))
         momentumNM = sqrt(2/(N+1)) * operator_at_site(momentum(singlebasis),N,1) * sin(pi*mode*1/(N+1))
 
@@ -68,110 +78,63 @@ module NormalModes
         return positionNM, momentumNM
     end
 
+    """
+    Frequency of the normal mode 'mode' of a harmonic chain with 'N' sites.
+    """    
     function FrequenceNormalMode(
-        N::Int64,
-        mode::Int64;
-        potential_factor::Float64 = 1.0
+        N::Int64,         # number of sites in chain
+        mode::Int64;      # index of normal mode
+        κ::Float64 = 1.0  # harmonic coupling strength
         )
-        """
-        Frequency of the 'k'-th normal mode of a harmonic FPUT chain with 'N' elements
-        """
-        return 2*sqrt(potential_factor)*sin(0.5*pi*mode/(N+1))
+        return 2*sqrt(κ)*sin(0.5*pi*mode/(N+1))
     end
 
-    function LaddersNormalMode(
-        N::Int64,  # number of chain elements
-        mode::Int64,  # level of normal mode
-        singlebasis::FockBasis; 
-        potential_factor::Float64 = 1.0
-        ) 
-        """
-        Creation and destruction operator of the 'k'-th normal mode of a harmonic FPUT chain with 'N' elements
-        """
-        Q, P = QuadraturesNormalMode(N, mode, singlebasis)
-        Ω = FrequenceNormalMode(N, mode; potential_factor=potential_factor)
-        destroyNM = sqrt(Ω/2)*Q + 1im/sqrt(2*Ω)*P
-        return destroyNM, dagger(destroyNM)
-    end
-
+    """
+    Hamiltonian of the normal mode 'mode' of a harmonic chain with 'N' sites. If a vector 'modes'
+    is passed, a vector of normal mode Hamiltonians is returned. 'singlebasis' is the Fock basis
+    of each single site of the chain.
+    """
     function HamiltonianNormalMode(
-        N::Int64,     # number of chain elements
-        mode::Int64,  # level of normal mode
-        singlebasis::FockBasis;
-        potential_factor::Float64 = 1.0,
-        build_by_quadratures::Bool = true
+        N::Int64,     # number of sites in chain
+        mode::Int64,  # index of normal mode
+        singlebasis::FockBasis;  
+        κ::Float64 = 1.0 # harmonic coupling strength
         )
 
-        if build_by_quadratures == true
-            Q, P = QuadraturesNormalMode(N,mode,singlebasis)
-            ω = FrequenceNormalMode(N,mode; potential_factor=potential_factor)
-            HamNM = 0.5 * (P*dagger(P) + ω^2*Q*dagger(Q))
-        else
-            A, At = LaddersNormalMode(N, mode, singlebasis; potential_factor=potential_factor)
-            idFPUT = operator_at_site(identityoperator(singlebasis), N, 1)
-            ω = FrequenceNormalMode(N,mode; potential_factor=potential_factor)
-            HamNM = ω*(At*A + 0.5*idFPUT)
-        end
+        Q, P = QuadraturesNormalMode(N,mode,singlebasis)
+        ω = FrequenceNormalMode(N,mode; κ=κ)
+        HamNM = 0.5 * (P*dagger(P) + ω^2*Q*dagger(Q))
 
         return (HamNM + dagger(HamNM))/2
     end
 
     function HamiltonianNormalMode(
-        N::Int64,  # number of chain elements
-        modes::Vector{Int64},  # level of normal mode
+        N::Int64,              # number of sites in chain
+        modes::Vector{Int64},  # indeces of normal modes
         singlebasis::FockBasis;
-        potential_factor::Float64=1.0
+        κ::Float64=1.0  # harmonic coupling strength
         )
 
         HamOps = Vector{Operator}()
         for k in modes
-            Ham = HamiltonianNormalMode(N, k, singlebasis; potential_factor=potential_factor)
+            Ham = HamiltonianNormalMode(N, k, singlebasis; κ=κ)
             push!(HamOps, Ham)
         end
 
         return HamOps
     end
 
-
-
-    function NumberoperatorNormalMode(
-        N::Int64,  # ...number of chain elements
-        mode::Int64,
-        singlebasis::FockBasis;
-        potential_factor::Float64 = 1.0
-        )
-
-        # idCHAIN = operator_at_site(identityoperator(singlebasis), N, 1)
-        # Q, P = QuadraturesNormalMode(N,mode,singlebasis)
-        # ω = FrequenceNormalMode(N,mode; potential_factor=potential_factor)
-        # return 0.5 * ( P^2/ω + ω*Q^2 - idCHAIN )
-
-        A, At = LaddersNormalMode(N, mode, singlebasis; potential_factor=potential_factor)
-        return At*A
-    end
-
-
-    function NumberoperatorNormalMode(
-        N::Int64,  # number of chain elements
-        modes::Vector{Int64},  # level of normal mode
-        singlebasis::FockBasis;
-        potential_factor::Float64=1.0
-        )
-
-        Noperators = Vector{Operator}()
-        for k in modes
-            Nop = NumberoperatorNormalMode(N, k, singlebasis; potential_factor=potential_factor)
-            push!(Noperators, Nop)
-        end
-
-        return Noperators
-    end
-
-
+    """
+    Normal mode displacement operator, i.e. the usual displacement operator yet with normal mode
+    quaratures of nomral mode 'mode' of a chain of 'N' sites. 'singlebasis' is the Fock basis of
+    each single site of the chain. 'x' is the displacement vector in the phase-space of the mode,
+    while 'ϕ' is the corresponding complex number for the complex plain representation of the
+    phase-space
+    """
     function DisplacementoperatorNormalMode(
-        x::Vector{Float64},
-        N::Int64,  # number of chain elements
-        mode::Int64,  # level of normal mode
+        x::Vector{Float64},  # normal mode phase-space displacement
+        N::Int64,            # number of sites in chain
+        mode::Int64,         # index of normal mode
         singlebasis::FockBasis
         )
         Q,P = QuadraturesNormalMode(N,mode,singlebasis)
@@ -180,9 +143,9 @@ module NormalModes
     end
 
     function DisplacementoperatorNormalMode(
-        ϕ::ComplexF64,
-        N::Int64,  # number of chain elements
-        mode::Int64,  # level of normal mode
+        ϕ::ComplexF64,  # normal mode phase-space displacement
+        N::Int64,       # number of sites in chain
+        mode::Int64,    # index of normal mode
         singlebasis::FockBasis
         )
 
@@ -192,11 +155,14 @@ module NormalModes
         return exp(-1im*transpose(Ω*x)*[Q,P])
     end
 
+    """
+    Excitation of a harmonic chain of 'N' sites in a normal mode coherent state |ϕ>.
+    """
     function coherentstateNM(
         singlebasis::FockBasis,
-        ϕ,
-        N::Int64;
-        mode::Int64 = 1
+        ϕ,         # normal mode phase-space displacement
+        N::Int64;  # number of sites in chain
+        mode::Int64 = 1 # index of normal mode
         )
 
         vacuumstateNM = fockstate(singlebasis,0)
@@ -208,30 +174,11 @@ module NormalModes
         return Dop*vacuumstateNM
     end
 
-    function fockstateNM(
-        singlebasis::FockBasis,
-        n,
-        N::Int64;
-        mode::Int64 = 1
-        )
 
-        CreateNM = LaddersNormalMode(N,mode,singlebasis)[1]
-
-        vacuumstateNM = fockstate(singlebasis,0)
-        for i in 2:N
-            vacuumstateNM = vacuumstateNM⊗fockstate(singlebasis,0)
-        end
-        return CreateNM^n * vacuumstateNM/norm(CreateNM^n * vacuumstateNM)
-
-    
-        # HamNM = HamiltonianNormalMode(N,mode,singlebasis)
-        # eigenvalsNM, eigenstatesNM = eigenstates(dense(HamNM))
-        # groundstateNM = eigenstatesNM[1]
-        # return CreateNM^n * groundstateNM/norm(CreateNM^n * groundstateNM)
-    end
-
-
-
+    """
+    computing mean values for an observable (or a collection of such) over a trajectory of
+    ket states or an ensemble of Monte-Carlo wave function trajectories
+    """
     function means(
         operator::Operator,
         trajectory::Vector{<:Ket}
